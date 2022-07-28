c79bdadd50e4d0a516b8306d0d0c4975aee88103
aaabhishekk <110158208+aaabhishekk@users.noreply.github.com>
2022-07-28 14:10:11 +0530

    Add files via upload

---
+++ social media folow/main.js
@@ -0,0 +1,31 @@
+
+
+const counter=document.querySelectorAll(".counter");
+
+// console.log(contaniner)
+
+counter.forEach((counter)=>{
+counter.innerHTML="0"
+
+const updateCounter = () => {
+
+
+const target= +counter.getAttribute("data-target")
+
+// console.log(typeof(target));
+const c= +counter.innerHTML
+const incre=target/200;
+console.log(incre);
+if(c<target){
+    counter.innerText=`${Math.ceil(c+incre)}`;
+
+    setTimeout(updateCounter,2)
+}else{
+    counter.innerText=target;
+}
+
+};
+
+updateCounter();
+
+})
